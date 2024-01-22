8553ded92aa56bcc8c41a16ab1918fe90daa4630
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-01-22 20:07:04 +0100

    Popular Jobs See all

--- containers/Homepage/PopularJobsContainer.js
+++ containers/Homepage/PopularJobsContainer.js
@@ -1,12 +1,32 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { JobFinderGeneralCard, JobFinderJobCard } from "../../components/Card";
+import { TECHJOBSDATA } from "../../constants/Data/DummyData";
 
 export default function PopularJobsContainer() {
-  return (
-    <View>
-      <Text>PopularJobsContainer</Text>
-    </View>
-  )
+  const renderJobFinderCards = () => {
+    return (
+      <View>
+        {TECHJOBSDATA.map((item) => (
+          <JobFinderJobCard
+            key={item.id}
+            jobTitle={item.jobTitle}
+            jobType={item.jobType}
+            icon={item.icon}
+            amount={item.amount}
+            country={item.country}
+          />
+        ))}
+      </View>
+    );
+  };
+
+  return <JobFinderGeneralCard>{renderJobFinderCards()}</JobFinderGeneralCard>;
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  columnWrapper: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+  },
+});
